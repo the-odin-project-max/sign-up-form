98df7bb4d5ee9027afdbb058996b795963e04332
Max <max.teiger@gmail.com>
2023-10-10 17:42:01 +0200

    feat(form): initalize form with basic checks

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+// function checkIdenticalPwd() {
+// 	var pwd = document.getElementById("password").value;
+// 	var pwdConfirm = document.getElementById("passwordConfirmed").value;
+// 	if (pwd != pwdConfirm) {
+// 		alert("Password and Confirm Password do not match!");
+// 		return false;
+// 	}
+// 	return true;
+// }
+
+// document.getElementById("submit-form").addEventListener("click", function (e) {
+// 	alert("Sign Up Successful!");
+// 	e.preventDefault();
+// 	if (checkIdenticalPwd()) {
+// 		alert("Sign Up Successful!");
+// 	}
+// });
