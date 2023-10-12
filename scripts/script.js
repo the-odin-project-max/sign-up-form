910ea6b76a2c6e4467a595bc090965d52b7f3df7
Max <max.teiger@gmail.com>
2023-10-12 11:18:09 +0200

    feat(js): add logic to form

--- scripts/script.js
+++ scripts/script.js
@@ -10,18 +10,38 @@ function checkIdenticalPwd() {
 	return true;
 }
 
+function checkIfFormComplete() {
+	var output = true;
+	document.querySelectorAll("input").forEach(function (input) {
+		if (input.value == "") {
+			input.classList.add("invalid");
+			output = false;
+		}
+	});
+	return output;
+}
+
 document.getElementById("submit-form").addEventListener("click", function (e) {
 	e.preventDefault();
+	if (!checkIfFormComplete()) {
+		alert("Please fill in all missing fields!");
+		return;
+	}
+
 	if (checkIdenticalPwd()) {
 		alert("Sign Up Successful!");
 	}
 });
 
-var test = document.getElementById("test");
-test.stat
-
 document.querySelectorAll("input").forEach(function (input) {
-	input.addEventListener("focus", function () {
+	input.addEventListener("input", function () {
 		this.classList.remove("invalid");
+		// alert(input.parentElement);
 	});
+
+	input.addEventListener("valid", function () {
+		this.classList.remove("invalid");
+		this.classList.add("valid");
+		// alert(input.parentElement);
+	}
 });
