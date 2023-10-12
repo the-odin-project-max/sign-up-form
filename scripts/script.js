f664045a7345a21508e43366c38ab5c927e772ab
Max <max.teiger@gmail.com>
2023-10-12 10:56:53 +0200

    feat(ui): change ui so we can resize the screen

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,27 @@
-// function checkIdenticalPwd() {
-// 	var pwd = document.getElementById("password").value;
-// 	var pwdConfirm = document.getElementById("passwordConfirmed").value;
-// 	if (pwd != pwdConfirm) {
-// 		alert("Password and Confirm Password do not match!");
-// 		return false;
-// 	}
-// 	return true;
-// }
+function checkIdenticalPwd() {
+	var pwd = document.getElementById("password");
+	var pwdConfirm = document.getElementById("passwordConfirmed");
+	if (pwd.value != pwdConfirm.value) {
+		pwd.classList.add("invalid");
+		pwdConfirm.classList.add("invalid");
+		alert("Password and Confirm Password do not match!");
+		return false;
+	}
+	return true;
+}
 
-// document.getElementById("submit-form").addEventListener("click", function (e) {
-// 	alert("Sign Up Successful!");
-// 	e.preventDefault();
-// 	if (checkIdenticalPwd()) {
-// 		alert("Sign Up Successful!");
-// 	}
-// });
+document.getElementById("submit-form").addEventListener("click", function (e) {
+	e.preventDefault();
+	if (checkIdenticalPwd()) {
+		alert("Sign Up Successful!");
+	}
+});
+
+var test = document.getElementById("test");
+test.stat
+
+document.querySelectorAll("input").forEach(function (input) {
+	input.addEventListener("focus", function () {
+		this.classList.remove("invalid");
+	});
+});
